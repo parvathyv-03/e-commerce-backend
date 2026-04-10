e38e37c182ac17ae0662459abb68cfbb57c94a71
parvathyv-03 <parvathyvof@gmail.com>
2026-04-10 21:51:47 +0530

    Register cart and wishlist in admin

--- backend/settings.py
+++ backend/settings.py
@@ -12,12 +12,13 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
